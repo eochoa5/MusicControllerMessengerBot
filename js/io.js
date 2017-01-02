a3aefebb79407687b666d32653eed000fc6c694b
eochoa5 <edwinochoa57@gmail.com>
2017-01-02 00:15:17 -0800

    Update io.js

--- js/io.js
+++ js/io.js
@@ -8,24 +8,13 @@ $(function(){
 		});
 
 		socket.on('new message', function(data){
-			//data.msg
-			//$container.html(data.msg);
-
 			var source=data.msg + "?enablejsapi=1&autoplay=1";
 			$container.html('<iframe width="100%" height="100%" src="'+source+'" frameborder="0"> </iframe>');
-			setTimeout(play, 3000);
-				
-				
+						
 		});
 
 });
 
-function play(){
-	jQuery("iframe").each(function() {
-  	jQuery(this)[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*')
-	});
-
-}
 function pause(){
 	jQuery("iframe").each(function() {
   	jQuery(this)[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
